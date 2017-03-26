6637781939fa16ef769605855a9ecb65ad64765e
iotool <roha.mbox@gmail.com>
2017-03-27 00:30:51 +0200

    Add files via upload

--- attiny13a_solar-control.ino
+++ attiny13a_solar-control.ino
@@ -1,7 +1,7 @@
 // ATtiny13a SolarControl
 // 
 // ADC3 = VCC der Solarzelle ueber 1.6V LED als Vref messen
-// ADC4 = optionale LED mit Blinkcode fuer Spannung (2.7/3.0/3.7/4.2V)
+// ADC4 = optionale LED mit Blinkcode fuer Spannung (2.6/3.0/3.6/4.2V)
 // PWM0 = Schalter ein ~ VCC > 3.6V / aus VCC < 2.6V
 // PWM1 = Schalter ein ~ VCC > 3.0V / aus VCC < 3.0V
 // 
@@ -11,7 +11,8 @@
 // Wenn der Kondensator unter 2.6V entladen wird, dann wird der Verbraucher ausgeschaltet und der Kondensator geladen.
 // Bei 3.0V Schaltet ein zweiter Schalter ein/aus und kann als LowPower-Signal vom Verbraucher genutzt werden.
 // 
-// Ladezeit Bewoelkung von 1.8V auf 3.6V ca. 13 Minuten fuer 0.47F 5.5V Supercap
+// Ladezeit bei Bewoelkung von 0.8V auf 3.6V ca. 5 Minuten fuer 0.47F 5.5V Supercap mit Solar 6V 110x60mm
+// 0.47F von 3.6V bis 2.6V entsprechen 470mAs
 // 
 // HARDWARE:
 // 1x ATtiny13a (Controller)
@@ -34,119 +35,235 @@
 // ATtiny13A 14.12.1 ADMUX – ADC Multiplexer Selection Register
 //
 // HISTORIE:
-// 2017-03-21  adjust ADC-VCC 2.6V / 3.7V
+// 2017-03-21(V1)  adjust ADC-VCC 2.6V / 3.7V
+// 2017-03-22(V3)  sleep, low level api, reduce bytecode
 // 
 // BOARD: ATtiny13, 1MHz disable BrownOut
 // PGM 1024 B | VAR 64 B | Bemerkung
-//  270 (26%) |  9 (14%) | leer
-//  412 (40%) |  9 (14%) | PinMode (Setup)
-//  554 (54%) |  9 (14%) | DigitalWrite (Setup)
-//  830 (81%) | 11 (17%) | getVCC
-//  852 (83%) | 11 (17%) | setSWITCH
-//  870 (84%) | 13 (20%) | if setSWITCH
-//  894 (87%) | 13 (20%) | if getVCC
-//  948 (92%) | 13 (20%) | setBlinken
-//  994 (97%) | 13 (20%) | setPWM1
+//  390 (38%) |  1 (1%)  | low level api
 
-#define __AVR_ATtiny13A__
-#define REFS0 6  // Reference Selection Bit = voltage reference for the ADC (0: Vcc, 1: Internal Voltage)
-#define ADLAR 5  // ADC Left Adjust Result
-#define MUX1  1  // Analog Channel Selection Bits
-#define MUX0  0  // Analog Channel Selection Bits
+#define _AVR_ATTINY13A_H_ 1  // iotn13a.h
+#define F_CPU 1200000UL      // delay cpu clock
+#include <avr/io.h>          // low level pinmode
+#include <avr/interrupt.h>   // timer interrupt for sleep
+#include <avr/sleep.h>       // sleep to reduce power
+#include <util/delay.h>      // low level delay
 
-#define DELAY_50MS 60    // 50 ms auf ATtiny13a 
-#define DELAY_100MS 120  // 100 ms auf ATtiny13a 
-#define DELAY_1S 1200    // 1 s auf ATtiny13a
-#define DELAY_ADC 60     // 50 ms auf ATtiny13a
+// interrupt timer
+#define SLEEPTIMER_16MS   WDTCR |= (0<<WDP3)|(0<<WDP2)|(0<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_32MS   WDTCR |= (0<<WDP3)|(0<<WDP2)|(0<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_64MS   WDTCR |= (0<<WDP3)|(0<<WDP2)|(1<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_125MS  WDTCR |= (0<<WDP3)|(0<<WDP2)|(1<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_250MS  WDTCR |= (0<<WDP3)|(1<<WDP2)|(0<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_500MS  WDTCR |= (0<<WDP3)|(1<<WDP2)|(0<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_1S     WDTCR |= (0<<WDP3)|(1<<WDP2)|(1<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_2S     WDTCR |= (0<<WDP3)|(1<<WDP2)|(1<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_4S     WDTCR |= (1<<WDP3)|(0<<WDP2)|(0<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_8S     WDTCR |= (1<<WDP3)|(0<<WDP2)|(0<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_START  WDTCR |= (1<<WDTIE); WDTCR |= (0<<WDE); sei(); set_sleep_mode(SLEEP_MODE_PWR_DOWN)
 
-#define ADC_26V 6237  // readAdc() 2.6V (gemessen 2.59V)
-#define ADC_27V 5970  // readAdc() 2.7V
-#define ADC_30V 5415  // readAdc() 3.0V (gemessen 3.02V)
-#define ADC_33V 4860  // readAdc() 3.3V
-#define ADC_36V 4590  // readAdc() 3.6V 
-#define ADC_37V 4568  // readAdc() 3.7V (gemessen 3.58V)
-#define ADC_45V 3690  // readAdc() 4.5V
+// delay
+#define DELAY_10MS   _delay_ms(12)
+#define DELAY_50MS   _delay_ms(60)
+#define DELAY_100MS  _delay_ms(120)
+#define DELAY_1S     _delay_ms(1200)
+#define DELAY_ADC    _delay_ms(6)
+#define DELAY_MORSE  _delay_ms(120)
 
-#define PIN_GETVCC 3  // ADC3=Pin3 Spannung der Solarzelle messen
-#define PIN_SWITCH 0  // PWM0=Pin0 Schalter fuer Verbraucher
-#define PIN_LED 4     // ADC4=Pin4 Signal-LED fuer Ladezustand
-#define PIN_CHARGE 1  // PWM1=Pin1 Schalter fuer Lade
+// pinMode(PBx,INPUT/OUTPUT)
+#define PINMODE_PB0_INPUT  DDRB &= ~(1 << PB0)
+#define PINMODE_PB1_INPUT  DDRB &= ~(1 << PB1)
+#define PINMODE_PB2_INPUT  DDRB &= ~(1 << PB2)
+#define PINMODE_PB3_INPUT  DDRB &= ~(1 << PB3)
+#define PINMODE_PB4_INPUT  DDRB &= ~(1 << PB4)
+#define PINMODE_PB0_OUTPUT  DDRB |= (1 << PB0)
+#define PINMODE_PB1_OUTPUT  DDRB |= (1 << PB1)
+#define PINMODE_PB2_OUTPUT  DDRB |= (1 << PB2)
+#define PINMODE_PB3_OUTPUT  DDRB |= (1 << PB3)
+#define PINMODE_PB4_OUTPUT  DDRB |= (1 << PB4)
 
-#define MODE_CHARGE 1   // aufladen auf 3.3V
-#define MODE_POWERON 2  // entladen auf 2.7V
+// digitalWrite(PBx,LOW/HIGH)
+#define DIGITALWRITE_PB0_LOW  PORTB &= ~(1 << PB0)
+#define DIGITALWRITE_PB1_LOW  PORTB &= ~(1 << PB1)
+#define DIGITALWRITE_PB2_LOW  PORTB &= ~(1 << PB2)
+#define DIGITALWRITE_PB3_LOW  PORTB &= ~(1 << PB3)
+#define DIGITALWRITE_PB4_LOW  PORTB &= ~(1 << PB4)
+#define DIGITALWRITE_PB0_HIGH  PORTB |= (1 << PB0)
+#define DIGITALWRITE_PB1_HIGH  PORTB |= (1 << PB1)
+#define DIGITALWRITE_PB2_HIGH  PORTB |= (1 << PB2)
+#define DIGITALWRITE_PB3_HIGH  PORTB |= (1 << PB3)
+#define DIGITALWRITE_PB4_HIGH  PORTB |= (1 << PB4)
 
-int lastADC = 0;  // letzte gemessene Spannung
-byte setSWITCH = MODE_CHARGE;  // letzter Ausgabemodus
+// digitalRead(PBx) == LOW
+#define DIGITALREAD_PB0_LOW    (PINB & (1 << PB0))
+#define DIGITALREAD_PB1_LOW    (PINB & (1 << PB1))
+#define DIGITALREAD_PB2_LOW    (PINB & (1 << PB2))
+#define DIGITALREAD_PB3_LOW    (PINB & (1 << PB3))
+#define DIGITALREAD_PB4_LOW    (PINB & (1 << PB4))
 
-int readAdc() { 
-  // http://www.avrfreaks.net/forum/please-big-help-attiny13-adc
-  // 
-  // Messbereich 2.7-3.3V
-  // 541.5/1024 = 1.6V/3.0V mit 1.6V LED als Vref
-  // 10x Messungen ~ 5415/1024 = 10*1.6V/3.0V
-  // 
-  // Berechnung:
-  // ADC <= 9350 ~ 1.3V
-  // ADC <= 6155 ~ 2.6V
-  // ADC <= 5970 ~ 2.7V
-  // ADC <= 5415 ~ 3.0V
-  // ADC <= 4860 ~ 3.3V
-  // ADC <= 4568 ~ 3.6V
-  // ADC <= 3690 ~ 4.5V
-  // 
-  ADCSRA |= (1 << ADEN);           // Enable ADC  
-  ADMUX |= (0 << REFS0);           // vcc reference
-  ADMUX = PIN_GETVCC;              // ADC3 PB.3
-  for (byte i=0; i<10; i++) 
-  {                                // ADC einschwingen
-    ADCSRA |= (1 << ADSC);         // Start Converstion
-    delay(DELAY_ADC);              // Wait for Vref to settle
+// analog digital converter
+#define ADC_DISABLE       ADCSRA &= ~(1 << ADEN)
+#define ADC_ENABLE        ADCSRA |= (1 << ADEN); ADCSRA |= (1 << ADSC); while (ADCSRA & (1 << ADSC))
+#define ADC1_PB2_SELECT   ADMUX |= (0 << REFS0); ADMUX |= (0 << MUX1)|(1 << MUX0)
+#define ADC2_PB4_SELECT   ADMUX |= (0 << REFS0); ADMUX |= (1 << MUX1)|(0 << MUX0)
+#define ADC3_PB3_SELECT   ADMUX |= (0 << REFS0); ADMUX |= (1 << MUX1)|(1 << MUX0)
+#define ADC_PREREAD       5
+#define ADC_READ          10
+
+// mapping readAdc() voltage
+#define ADC_26V 6240 // readAdc() 2.6V (2.61V)
+#define ADC_27V 5995 // readAdc() 2.7V
+#define ADC_30V 5430 // readAdc() 3.0V (3.05V)
+#define ADC_32V 5145 // readAdc() 3.2V
+#define ADC_33V 5000 // readAdc() 3.3V (3.65V)
+#define ADC_36V 4600 // readAdc() 3.6V
+#define ADC_42V 4000 // readAdc() 4.2V
+#define ADC_45V 3727 // readAdc() 4.5V
+#define ADC_51V 3340 // readAdc() 5.1V 
+
+// void setup() {}  // Use main() instat of setup()/loop()
+// void loop() {}   // to reduce bytecode from 270 to 44 byte
+
+void morseNumber(byte a, byte b, byte c, byte d, byte e) {
+  if (a <= 1) {DIGITALWRITE_PB4_HIGH; DELAY_MORSE; if (a >= 1) DELAY_MORSE; DIGITALWRITE_PB4_LOW; DELAY_MORSE;}
+  if (b <= 1) {DIGITALWRITE_PB4_HIGH; DELAY_MORSE; if (b >= 1) DELAY_MORSE; DIGITALWRITE_PB4_LOW; DELAY_MORSE;}
+  if (c <= 1) {DIGITALWRITE_PB4_HIGH; DELAY_MORSE; if (c >= 1) DELAY_MORSE; DIGITALWRITE_PB4_LOW; DELAY_MORSE;}
+  if (d <= 1) {DIGITALWRITE_PB4_HIGH; DELAY_MORSE; if (d >= 1) DELAY_MORSE; DIGITALWRITE_PB4_LOW; DELAY_MORSE;}
+  if (e <= 1) {DIGITALWRITE_PB4_HIGH; DELAY_MORSE; if (e >= 1) DELAY_MORSE; DIGITALWRITE_PB4_LOW; DELAY_MORSE;}
+  DELAY_MORSE; DELAY_MORSE; 
+}
+
+void morseDigit(byte digit) {
+  if (digit == 0) morseNumber(1,1,1,1,1); // 0
+  if (digit == 1) morseNumber(0,1,1,1,1); // 1
+  if (digit == 2) morseNumber(0,0,1,1,1); // 2
+  if (digit == 3) morseNumber(0,0,0,1,1); // 3
+  if (digit == 4) morseNumber(0,0,0,0,1); // 4
+  if (digit == 5) morseNumber(0,0,0,0,0); // 5
+  if (digit == 6) morseNumber(1,0,0,0,0); // 6
+  if (digit == 7) morseNumber(1,1,0,0,0); // 7
+  if (digit == 8) morseNumber(1,1,1,0,0); // 8
+  if (digit == 9) morseNumber(1,1,1,1,0); // 9  
+}
+
+void morseCode(int value) {
+  byte morse_1000 = 0;
+  byte morse_100 = 0;
+  byte morse_10 = 0;
+  byte morse_1 = 0;
+  while(value >= 1000) {
+    morse_1000++;
+    value-=1000;
+  }
+  while(value >= 100) {
+    morse_100++;
+    value-=100;
+  }
+  while(value >= 10) {
+    morse_10++;
+    value-=10;
+  }
+  morse_1 = value;
+  morseDigit(morse_1000);
+  morseDigit(morse_100);
+  morseDigit(morse_10);
+  morseDigit(morse_1);
+  DELAY_MORSE; DELAY_MORSE; DELAY_MORSE; 
+}
+
+int readADC() {
+  byte i;
+  for (i=0; i<ADC_PREREAD; i++) {  // ADC einschwingen
+    ADCSRA |= (1 << ADSC);         // start converstion
+    DELAY_ADC;                     // wait for Vref to settle
     while((ADCSRA & 0x40) !=0){};  // wait for conv complete
   }
-  int adcValue = 0;
-  for (byte i=0; i<10; i++) 
-  {                                // mehrere Messungen
-    ADCSRA |= (1 << ADSC);         // Start Converstion
-    delay(DELAY_ADC);              // Wait for Vref to settle
+  int sumAdc = 0;
+  for (byte i=0; i<ADC_READ; i++) 
+  {                                // ADC Messungen
+    ADCSRA |= (1 << ADSC);         // start converstion
+    DELAY_ADC;                     // wait for Vref to settle
     while((ADCSRA & 0x40) !=0){};  // wait for conv complete
-    adcValue = adcValue + ADC;
+    sumAdc += ADC;
   }
-  return adcValue;
+  return sumAdc;
 }
 
-void setup() {
-  pinMode(PIN_SWITCH, OUTPUT);     // PIN0 = Schalter
-  pinMode(PIN_LED, OUTPUT);        // PIN4 = LED
-  pinMode(PIN_CHARGE, OUTPUT);     // PIN1 = Ladegeraet
-  // digitalWrite(PIN_SWITCH, LOW);   // Schalter aus
-  // digitalWrite(PIN_LED, LOW);      // LED aus
-  // digitalWrite(PIN_CHARGE, LOW);   // Ladegeraet aus
+byte ticker = 0;    // count 64 ms sleep interval
+
+ISR(WDT_vect) {
+  ticker++;
 }
 
-void loop() {
-  lastADC = readAdc();
+int main() {
 
-  if (lastADC < ADC_36V)             // if GETVCC > 3.6V
-    setSWITCH = MODE_POWERON;        // then PowerOn
-  else if (lastADC > ADC_26V)        // if GETVCC < 2.6V
-    setSWITCH = MODE_CHARGE;         // then Charge
+  // setup - once
 
-  if (lastADC < ADC_30V)             // if GETVCC > 3.0V
-    digitalWrite(PIN_CHARGE, HIGH);  // then Charge On
-  else                               // else LowPower
-    digitalWrite(PIN_CHARGE, LOW);   // then Charge Off
+  PINMODE_PB0_OUTPUT;
+  PINMODE_PB1_OUTPUT;
+  PINMODE_PB2_OUTPUT;
+  PINMODE_PB3_OUTPUT;
+  PINMODE_PB4_OUTPUT;
 
-  if (setSWITCH >= MODE_POWERON)     // if Charged
-    digitalWrite(PIN_SWITCH, HIGH);  // then PowerOn
-  else                               // else Discharged
-    digitalWrite(PIN_SWITCH, LOW);   // then PowerOff
+  SLEEPTIMER_64MS;
+  SLEEPTIMER_START;
+  ADC_DISABLE;
 
-  while((lastADC+=555)<6710) {
-    digitalWrite(PIN_LED, HIGH);     // LED ein
-    delay(DELAY_50MS);
-    digitalWrite(PIN_LED, LOW);      // LED aus
-    delay(DELAY_100MS);
+  // loop
+
+  int lastVCC = 0;
+  ticker = 0;
+
+  while(1) {
+    // endless loop with timer based slots
+    // ticker increments every 64ms by interrupt
+    switch(ticker) {
+      case 1:
+        // messure vcc
+        PINMODE_PB3_INPUT;        // use PB3 as input
+        ADC3_PB3_SELECT;          // use ADC3 for read
+        ADC_ENABLE;               // enable ADC hardware
+        lastVCC = readADC();      // get VCC by know Vref of LED
+        ADC_DISABLE;              // reduce power
+        PINMODE_PB3_OUTPUT;       // reduce power
+        // debug vcc
+        // morseCode(lastVCC);
+        // switch output
+        if (lastVCC < ADC_30V) { 
+          DIGITALWRITE_PB1_HIGH;  // VCC > 3.0V = switch on
+          ticker = 6;             // 2x blink code (next ticker 7)
+        } else {
+          DIGITALWRITE_PB1_LOW;   // VCC < 3.0V = switch off
+          ticker = 9;             // 1x blink code (next ticker 10)
+        }
+        if (lastVCC < ADC_36V) { 
+          DIGITALWRITE_PB0_HIGH;  // VCC > 3.6V = switch on
+          ticker = 3;             // 3x blink code (next ticker 4)
+        } else if (lastVCC > ADC_26V) {
+          DIGITALWRITE_PB0_LOW;  // VCC < 2.6V = switch off
+          ticker = 12;           // blink disable (next ticker 13)
+        }
+        if (lastVCC < ADC_42V) { // VCC > 4.2V
+          ticker = 1;            // 4x blink code (next ticker 2)
+        }
+        break;
+      case  2: DIGITALWRITE_PB4_HIGH; break;  case  3: DIGITALWRITE_PB4_LOW; break;
+      case  5: DIGITALWRITE_PB4_HIGH; break;  case  6: DIGITALWRITE_PB4_LOW; break;
+      case  8: DIGITALWRITE_PB4_HIGH; break;  case  9: DIGITALWRITE_PB4_LOW; break;
+      case 11: DIGITALWRITE_PB4_HIGH; break;  case 12: DIGITALWRITE_PB4_LOW; break;
+      case 14:
+        if (DIGITALREAD_PB3_LOW) {} 
+        else {                   // fast refresh VCC every second (because switch is on)
+          ticker = 31;           // 32x 64ms = 2048 ms left until next ticker overflow
+        }
+        break;
+      default:
+        if (ticker >= 63)        // refresh VCC after 4032 ms sleep
+          ticker = 0;            // ticker overflow every 2.5 seconds
+        break;
+    }
+    sleep_mode();                // power down sleep mode (0.007 mA)
   }
-
-  delay(DELAY_1S);
+  
 }
+
